4347af91d62cea485af279ed282b6b686c46d83c
frandaniele <62730014+frandaniele@users.noreply.github.com>
2020-06-17 23:39:14 -0300

    clases de objetos de GUI
    probables clases de objetos para dibujar las diversas pantallas

---
+++ GUI_client/input_boxes.py
@@ -0,0 +1,49 @@
+import pygame as pg
+from colores import *
+
+class InputBox:
+
+    def __init__(self, x, y, w, h, text=''):
+        self.font = pg.font.SysFont('comicsansms', 32)
+        self.rect = pg.Rect(x, y, w, h)
+        self.color_inactive = blue
+        self.color_active = bright_blue
+        self.color = self.color_inactive
+        self.text = text
+        self.txt_surface = self.font.render(text, True, self.color)
+        self.active = False
+
+    def handle_event(self, event):
+        if event.type == pg.MOUSEBUTTONDOWN:
+            # If the user clicked on the input_box rect.
+            if self.rect.collidepoint(event.pos):
+                # Toggle the active variable.
+                self.active = not self.active
+            else:
+                self.active = False
+            # Change the current color of the input box.
+            self.color = self.color_active if self.active else self.color_inactive
+        if event.type == pg.KEYDOWN:
+            if self.active:
+                if event.key == pg.K_RETURN:
+                    print(self.text)
+                    self.text = ''
+                elif event.key == pg.K_BACKSPACE:
+                    self.text = self.text[:-1]
+                else:
+                    self.text += event.unicode
+                # Re-render the text.
+                self.txt_surface = self.font.render(self.text, True, self.color)
+
+    def update(self):
+        # Resize the box if the text is too long.
+        width = max(200, self.txt_surface.get_width()+10)
+        self.rect.w = width
+
+    def draw(self, screen):
+        pg.draw.rect(screen, red, self.rect)
+        # Blit the text.
+        screen.blit(self.txt_surface, (self.rect.x+5, self.rect.y+5))
+        # Blit the rect.
+        pg.draw.rect(screen, self.color, self.rect, 2)
+        
---
+++ GUI_client/main.py
@@ -0,0 +1,6 @@
+import pygame
+from colores import *
+from screens import Screen
+from texts import Text
+from buttons import Button
+from input_boxes import InputBox
